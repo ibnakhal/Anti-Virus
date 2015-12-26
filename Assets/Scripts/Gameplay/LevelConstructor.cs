74ad7ede2227ccbaa8e6afd0ce88c787abc65129
MrPotatoSack <ibznakhal@gmail.com>
2015-12-25 23:06:37 -0800

    fixed problem with prefab not being textured in instantiation as well as rigidbody errors; prototyping random room placement.

---
+++ Assets/Scripts/Gameplay/LevelConstructor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelConstructor : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] LevelSection;
+    [SerializeField]
+    private GameObject[] LevelPoints;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        LevelPoints = GameObject.FindGameObjectsWithTag("LevelPoint");
+	
+        for(int x=0; x<LevelPoints.Length; x++)
+        {
+            int y = Random.Range(0, LevelSection.Length);
+            Instantiate(LevelSection[y], LevelPoints[x].transform.position, Quaternion.identity);
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/UI/RoomTexter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomTexter : MonoBehaviour {
+
+    [SerializeField]
+    private Material masterMat;
+    [SerializeField]
+    private GameObject[] meshPieces;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+        StartCoroutine(StartUp());
+
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+
+    public IEnumerator StartUp()
+    {
+        yield return new WaitForEndOfFrame();
+
+        meshPieces = GameObject.FindGameObjectsWithTag("wall");
+
+        for (int x = 0; x < meshPieces.Length; x++)
+        {
+            meshPieces[x].GetComponent<Renderer>().material = masterMat;
+        }
+
+    }
+}
