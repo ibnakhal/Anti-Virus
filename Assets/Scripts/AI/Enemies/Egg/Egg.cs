0a3801d18130eb5081007440bfcdcd1ee60e9421
ibnakhal <inakhal2@180-705-01.students.aac.academyart.edu>
2016-02-07 23:53:05 -0800

    egg now spawns enemies

---
+++ Assets/Scripts/AI/Enemies/Egg/Egg.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Egg : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] spawnRange;
+    [SerializeField]
+    private GameObject sparkle;
+
+    public void OnTriggerEnter()
+    {
+        int rando = Random.Range(0, spawnRange.Length);
+        Instantiate(sparkle, this.transform.position, this.transform.rotation);
+        Instantiate(spawnRange[rando], this.transform.position, this.transform.rotation);
+        Destroy(this.gameObject);
+
+    }
+
+}
